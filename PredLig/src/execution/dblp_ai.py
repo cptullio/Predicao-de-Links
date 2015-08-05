185e2569315eea885cd0316725867e4df9470dcc
Carlos Muniz <cptullio@gmail.com>
2015-08-05 20:15:27 -0300

    subindo execucao do dblp 2000 a 2014

--- PredLig/src/execution/dblp_ai.py
+++ PredLig/src/execution/dblp_ai.py
@@ -13,12 +13,13 @@ from analysing.Analyse import Analyse
 
 
 if __name__ == '__main__':
-    util = ParameterUtil('data/parameter_dblp_ai_bc.txt')
+    
+    util = ParameterUtil('data/formatado/dblp_ai_2000_2014/parameter_dblp_ai_bc.txt')
     #format = Formating(util.original_file, util.graph_file)
     #format.readingOrginalDataset()
     #format.saveGraph()
     myparams = Parameterization(util.top_rank, util.distanceNeighbors,util.lengthVertex, util.t0, util.t0_, util.t1, util.t1_, util.FeaturesChoiced, util.graph_file, util.trainnig_graph_file, util.test_graph_file, util.decay)
-    selecting = VariableSelection(myparams.trainnigGraph, util.nodes_notlinked_file)
-    calc = Calculate(myparams, util.nodes_notlinked_file, util.calculated_file, util.ordered_file)
-    calc.orderingCalculate()
-    analyse = Analyse(myparams, util.ordered_file, util.analysed_file)
+    #selecting = VariableSelection(myparams.trainnigGraph, util.nodes_notlinked_file)
+    calc = Calculate(myparams, util.nodes_notlinked_file, util.calculated_file, util.ordered_file, util.maxmincalculated_file)
+    #calc.orderingCalculate()
+    #analyse = Analyse(myparams, util.ordered_file, util.analysed_file)
