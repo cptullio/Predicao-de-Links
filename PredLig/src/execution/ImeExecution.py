fb2351f9ebf825b6a098f9aba9952bf55e45a361
Carlos Muniz <cptullio@gmail.com>
2015-08-17 08:05:19 -0300

    Modificando IMEExecution para manter os resultado para analise futura

--- PredLig/src/execution/ImeExecution.py
+++ PredLig/src/execution/ImeExecution.py
@@ -16,11 +16,11 @@ from calculating.CalculateMultiProcessing import CalculateMultiProcessing
 
 
 if __name__ == '__main__':
-    util = ParameterUtil('data/formatado/completo/duarte/config_ronaldo.txt')
+    util = ParameterUtil('data/formatado/ronaldo/config_ronaldo.txt')
     
-    #myformat = DuarteFormattingOnlyIME(util.graph_file)
-    #myformat.readingOrginalDataset()
-    #myformat.saveGraph()
+    myformat = DuarteFormattingOnlyIME(util.graph_file)
+    myformat.readingOrginalDataset()
+    myformat.saveGraph()
         
     myparams = Parameterization(util.top_rank, util.distanceNeighbors,util.lengthVertex, util.t0, util.t0_, util.t1, util.t1_, util.FeaturesChoiced, util.graph_file, util.trainnig_graph_file, util.test_graph_file, util.decay)
     myparams.generating_Training_Graph()
