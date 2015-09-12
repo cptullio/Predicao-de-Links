00d261455a5f7f7665ac5773560b540dbe4c52d0
Carlos Muniz <cptullio@gmail.com>
2015-09-12 08:21:07 -0300

    Excluindo arquivos que possam confundir os usuários.

--- PredLig/src/execution/teste.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on 12 de ago de 2015
-
-@author: CarlosPM
-'''
-from execution import MinhaClassMultiProcess
-
-if __name__ == '__main__':
-    c = MinhaClassMultiProcess.MyClass('step3_nodesnotlinked_Duarte_ts.txt')
