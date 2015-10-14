69332da599987d9e3b6655bd0fbeaa89f03cb1e2
Carlos Pedro Monaco Tullio Muniz <cptullio@hotmail.com>
2015-10-14 00:02:34 -0300

    Executando cond-mat without-weights na máquina do TCE

--- PredLig/src/execution/arxiv/Step03.py
+++ PredLig/src/execution/arxiv/Step03.py
@@ -10,8 +10,10 @@ from parametering.Parameterization import Parameterization
 from calculating.VariableSelection import VariableSelection
 
 if __name__ == '__main__':
-    util = ParameterUtil(parameter_file = 'data/formatado/arxiv/nowell_astroph_1994_1999.txt')
-    myparams = Parameterization(util.keyword_decay, util.lengthVertex, util.t0, util.t0_, util.t1, util.t1_, util.FeaturesChoiced, util.graph_file, util.trainnig_graph_file, util.test_graph_file, util.decay)
+    util = ParameterUtil(parameter_file = 'data/formatado/arxiv/nowell_condmat_1994_1999/config/configuration.txt')
+    myparams = Parameterization(t0 = util.t0, t0_ = util.t0_, t1 = util.t1, t1_ = util.t1_, 
+                                filePathGraph = util.graph_file, filePathTrainingGraph = util.trainnig_graph_file, filePathTestGraph = util.test_graph_file, decay = util.decay, domain_decay = util.domain_decay, min_edges = util.min_edges, scoreChoiced = util.ScoresChoiced, weightsChoiced = util.WeightsChoiced, weightedScoresChoiced = util.WeightedScoresChoiced, FullGraph = None)
+
     myparams.generating_Training_Graph()
     selection = VariableSelection(myparams.trainnigGraph, util.nodes_notlinked_file,util.min_edges)
     
