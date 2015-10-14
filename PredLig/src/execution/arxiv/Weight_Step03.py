8679a89d4919f6bd228f25623fb85888cea00072
Carlos Muniz <cptullio@gmail.com>
2015-10-14 05:58:37 -0300

    subindo configuração final de execução de Pesos com condmat

--- PredLig/src/execution/arxiv/Weight_Step03.py
+++ PredLig/src/execution/arxiv/Weight_Step03.py
@@ -10,10 +10,11 @@ from analysing.Analyse import Analyse
 from calculating.CalculateWeights import CalculateWeights
 
 if __name__ == '__main__':
-    util = ParameterUtil(parameter_file = 'data/formatado/arxiv/nowell_astroph_1994_1999/config/configuration.txt')
+    util = ParameterUtil(parameter_file = 'data/formatado/arxiv/nowell_condmat_1994_1999/config/configuration_weights.txt')
+       
     
     myparams = Parameterization(t0 = util.t0, t0_ = util.t0_, t1 = util.t1, t1_ = util.t1_, 
-                                filePathGraph = util.graph_file, filePathTrainingGraph = util.trainnig_graph_file, filePathTestGraph = util.test_graph_file, decay = util.decay, domain_decay = util.domain_decay, min_edges = util.min_edges, scoreChoices = util.ScoresChoiced, weightsChoiced = util.WeightsChoiced, weightedScoresChoiced = util.WeightedScoresChoiced, FullGraph = None)
+                                filePathGraph = util.graph_file, filePathTrainingGraph = util.trainnig_graph_file, filePathTestGraph = util.test_graph_file, decay = util.decay, domain_decay = util.domain_decay, min_edges = util.min_edges, scoreChoiced = util.ScoresChoiced, weightsChoiced = util.WeightsChoiced, weightedScoresChoiced = util.WeightedScoresChoiced, FullGraph = None)
 
     myparams.generating_Test_Graph()
     calc = CalculateWeights(preparedParameter = myparams, filepathNodesNotLinked = util.nodes_notlinked_file, filepathResult = util.calculated_file, filePathOrdered = util.ordered_file, filepathMaxMinCalculated = util.maxmincalculated_file)
--- PredLig/src/execution/arxiv/Weight_Step04.py
+++ PredLig/src/execution/arxiv/Weight_Step04.py
@@ -13,10 +13,13 @@ from calculating.Calculate import Calculate
 from calculating.CalculateWeights import CalculateWeights
 
 if __name__ == '__main__':
-    util = ParameterUtil(parameter_file = 'data/formatado/arxiv/nowell_astroph_1994_1999/config/configuration.txt')
+    util = ParameterUtil(parameter_file = 'data/formatado/arxiv/nowell_condmat_1994_1999/config/configuration_weights.txt')
+       
     
     myparams = Parameterization(t0 = util.t0, t0_ = util.t0_, t1 = util.t1, t1_ = util.t1_, 
-                                filePathGraph = util.graph_file, filePathTrainingGraph = util.trainnig_graph_file, filePathTestGraph = util.test_graph_file, decay = util.decay, domain_decay = util.domain_decay, min_edges = util.min_edges, scoreChoices = util.ScoresChoiced, weightsChoiced = util.WeightsChoiced, weightedScoresChoiced = util.WeightedScoresChoiced, FullGraph = None)
+                                filePathGraph = util.graph_file, filePathTrainingGraph = util.trainnig_graph_file, filePathTestGraph = util.test_graph_file, decay = util.decay, domain_decay = util.domain_decay, min_edges = util.min_edges, scoreChoiced = util.ScoresChoiced, weightsChoiced = util.WeightsChoiced, weightedScoresChoiced = util.WeightedScoresChoiced, FullGraph = None)
+
+    
     myparams.generating_Training_Graph()
     myparams.generating_Test_Graph()
     print "Trainning Period:", myparams.t0, " - ", myparams.t0_
