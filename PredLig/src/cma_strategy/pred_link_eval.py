1f6218c159dcb123ab98d56be514948a42907ad8
Carlos Muniz <cptullio@gmail.com>
2015-12-22 07:23:39 -0200

    subindo

--- PredLig/src/cma_strategy/pred_link_eval.py
+++ PredLig/src/cma_strategy/pred_link_eval.py
@@ -9,7 +9,7 @@ class SFrame:
     
     #util = ParameterUtil(parameter_file = 'data/formatado/arxiv/nowell_astroph_1994_1999/AllExecutionScores/configToAG.txt')
     #util = ParameterUtil(parameter_file = 'data/configuration/arxiv/exemplo_1994_1999/CombinationLinear/configToAG.txt')
-    util = ParameterUtil(parameter_file = 'data/configuration/arxiv/condmat_1994_1999/CombinationLinear/config.txt')
+    util = ParameterUtil(parameter_file = 'data/configuration/arxiv/condmat_1994_1999/CombinationLinear/configToAG.txt')
     
     
     myparams = Parameterization(t0 = util.t0, t0_ = util.t0_, t1 = util.t1, t1_ = util.t1_, linear_combination=util.linear_combination,
