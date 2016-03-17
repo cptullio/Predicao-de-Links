cf72db4bfaccd0bf2cbec8c87bcb01729bbcb565
Carlos Muniz <cptullio@gmail.com>
2016-03-17 08:07:24 -0300

    subindo o codigo do Jayme

--- PredLig/src/calculating/VariableSelection.py
+++ PredLig/src/calculating/VariableSelection.py
@@ -91,7 +91,16 @@ class VariableSelection(object):
             FormatingDataSets.printProgressofEvents(element, totalnodesOrdered, "Checking Node not liked: ")
             others =  set(n for n in nodesOrdered if n > node)
             for otherNode in others:
-                if (not self.graph.has_edge(node, otherNode)):
+                if self.USE_MAX_NUMBER_OF_PEOPLE_BETWEEN == True:
+                    if (not self.graph.has_edge(node, otherNode)):
+                        if networkx.has_path(self.graph, node, otherNode):
+                            tamanho_caminho = len(networkx.shortest_path(self.graph, node, otherNode)) - 2
+                            #print "%s ate %s: %s" %(node1, other_node,tamanho_caminho)
+                            #print repr(networkx.shortest_path(graph, node1, other_node));
+                            if ( tamanho_caminho > 0 ) and (tamanho_caminho <= self.MAX_NUMBER_OF_PEOPLE_BETWEEN * 2 + 1 ): # -2 porque inclui o inicio e fim
+                                print "adicionando %s - %s" %(node, otherNode)
+                                results.append([node, otherNode])
+                else:
                     results.append([node, otherNode])
                 
         print "getting pair of nodes that is not liked finished", datetime.today()
@@ -115,11 +124,13 @@ class VariableSelection(object):
         return results
     
 
-    def __init__(self, graph, min_papers = 1):
+    def __init__(self, graph, min_papers = 1, USE_MAX_NUMBER_OF_PEOPLE_BETWEEN = True, MAX_NUMBER_OF_PEOPLE_BETWEEN = 1000):
         
         self.graph = graph
         self.min_papers = min_papers
-        #self.MAX_NUMBER_OF_PEOPLE_BETWEEN = MAX_NUMBER_OF_PEOPLE_BETWEEN
+        self.USE_MAX_NUMBER_OF_PEOPLE_BETWEEN = USE_MAX_NUMBER_OF_PEOPLE_BETWEEN
+        self.MAX_NUMBER_OF_PEOPLE_BETWEEN = MAX_NUMBER_OF_PEOPLE_BETWEEN
+        
         #myfile = Formating.get_abs_file_path(filepathNodesToCalculate)
         #if not os.path.exists(myfile):
         #    with open(myfile, 'w') as fnodes:
