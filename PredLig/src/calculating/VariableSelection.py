835f9843bbda523f6f320d30b82b174173ac9f8a
Carlos Pedro Monaco Tullio Muniz <cptullio@hotmail.com>
2016-03-23 15:10:41 -0300

    subindo acerto de versão.

--- PredLig/src/calculating/VariableSelection.py
+++ PredLig/src/calculating/VariableSelection.py
@@ -30,54 +30,7 @@ class VariableSelection(object):
         return result
             
         
-     # Modified by Andre and Jayme  
-    def get_pair_nodes_not_linked_withPath(self, graph, file, min_papers):
-        print "Starting getting pair of nodes that is not liked", datetime.today()
-        results = []
-        nodesinGraph =set(n for n,d in graph.nodes(data=True) if d['node_type'] == 'N')
-        currentNodes = set()
-        for n in nodesinGraph:
-            
-            papers = set(networkx.all_neighbors(graph, n))
-            print papers
-            if (len(papers) >= min_papers):
-                currentNodes.add(n)
-        
-        print 'qty of authors: ', len(currentNodes)
-        nodesOrdered = sorted(currentNodes)
-        element = 0
-        totalnodesOrdered = len(nodesOrdered)
-        for node1 in nodesOrdered:
-            element = element+1
-            FormatingDataSets.printProgressofEvents(element, totalnodesOrdered, "Checking Node not liked: ")
-            
-            others =  set(n for n in nodesOrdered if n > node1)
-            notLinked = set()
-            for other_node in others:
-                if len(set(networkx.common_neighbors(graph, node1, other_node))) == 0:
-                    #notLinked.add(other_node) # como estava antes
-                    # esse if abaixo verifica se estao perto
-                    if networkx.has_path(graph, node1, other_node):
-                        tamanho_caminho = len(networkx.shortest_path(graph, node1, other_node)) - 2
-                        #print "%s ate %s: %s" %(node1, other_node,tamanho_caminho)
-                        #print repr(networkx.shortest_path(graph, node1, other_node));
-                        if ( tamanho_caminho > 0 ) and (tamanho_caminho <= self.MAX_NUMBER_OF_PEOPLE_BETWEEN * 2 + 1 ): # -2 porque inclui o inicio e fim
-                            print "adicionando %s - %s" %(node1, other_node)
-                            notLinked.add(other_node)
-            if len(notLinked) > 0:
-                results.append([node1, notLinked])
-            if element % 2000 == 0:
-                for item in results:
-                    file.write(str(item[0]) + '\t' +  repr(item[1]) + '\n')
-                results = []
-                
-        for item in results:
-            file.write(str(item[0]) + '\t' +  repr(item[1]) + '\n')
-        results = []
-            
-        print "getting pair of nodes that is not liked finished", datetime.today()
-
-
+  
     def get_pair_nodes_not_linked(self):
         print "Starting getting pair of nodes that is not liked", datetime.today()
         results = []
@@ -94,10 +47,12 @@ class VariableSelection(object):
                 if self.USE_MAX_NUMBER_OF_PEOPLE_BETWEEN == True:
                     if (not self.graph.has_edge(node, otherNode)):
                         if networkx.has_path(self.graph, node, otherNode):
-                            tamanho_caminho = len(networkx.shortest_path(self.graph, node, otherNode)) - 2
+                            shortestPathResult = networkx.shortest_path(self.graph, node, otherNode)
+                            print shortestPathResult
+                            tamanho_caminho = len(shortestPathResult) - 2
                             #print "%s ate %s: %s" %(node1, other_node,tamanho_caminho)
                             #print repr(networkx.shortest_path(graph, node1, other_node));
-                            if ( tamanho_caminho > 0 ) and (tamanho_caminho <= self.MAX_NUMBER_OF_PEOPLE_BETWEEN * 2 + 1 ): # -2 porque inclui o inicio e fim
+                            if ( tamanho_caminho > 0 ) and (tamanho_caminho <= self.MAX_NUMBER_OF_PEOPLE_BETWEEN ): # -2 porque inclui o inicio e fim
                                 print "adicionando %s - %s" %(node, otherNode)
                                 results.append([node, otherNode])
                 else:
@@ -124,7 +79,7 @@ class VariableSelection(object):
         return results
     
 
-    def __init__(self, graph, min_papers = 1, USE_MAX_NUMBER_OF_PEOPLE_BETWEEN = True, MAX_NUMBER_OF_PEOPLE_BETWEEN = 1000):
+    def __init__(self, graph, min_papers = 1, USE_MAX_NUMBER_OF_PEOPLE_BETWEEN = True, MAX_NUMBER_OF_PEOPLE_BETWEEN = 2):
         
         self.graph = graph
         self.min_papers = min_papers
