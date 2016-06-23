c10ca62c9b6913ccc44aa89e73321663728143d4
Carlos Pedro Monaco Tullio Muniz <cptullio@hotmail.com>
2016-06-23 17:38:55 -0300

    Subindo versao com o primeiro teste de grafo ponderado

--- PredLig/src/execution/weightedgraph/FullExecutionGraphNowell.py
+++ PredLig/src/execution/weightedgraph/FullExecutionGraphNowell.py
@@ -14,6 +14,63 @@ import datetime
 from calculating.NodeSelection import NodeSelection
 from calculating.CalculatingTogetherOnlyNowell import CalculatingTogetherOnlyNowell
 from pydblite import Base
+import numpy
+import os
+
+def all_neighbors(graph, node):
+    return set(networkx.all_neighbors(graph, node))
+    
+def get_common_neighbors(graph, node1, node2):
+    neighbors_node_1 = all_neighbors(node1)
+    neighbors_node_2 = all_neighbors(node2)
+    return neighbors_node_1.intersection(neighbors_node_2)    
+    
+def get_ObjectsofLinks(graph, node1, node2):
+    T3 = list(edge for n1, n2, edge in graph.edges([node1, node2], data=True) if ((n1 == node1 and n2 == node2) or (n1 == node2 and n2 == node1)) )
+    return  T3
+    
+def get_jacard_domain(bagofWordsNode1, bagofWordsNode2):
+    f = (float)(len(bagofWordsNode1.intersection(bagofWordsNode2)))
+    x = (float)(len(bagofWordsNode1.union(bagofWordsNode2)))
+    if x == 0:
+        return 0
+    return f/x
+
+def get_partOfWeightCalculating(graph, database, pair, cn):
+    primeiropar = sorted([str(pair[0]),str(cn)])
+    segundopar = sorted([str(pair[1]),str(cn)])
+    weightsA = database._pair[str(primeiropar[0])+';'+str(primeiropar[1])][0]
+    weightsB = database._pair[str(segundopar[0])+';'+str(segundopar[1])][0]
+    
+    cnWts02 = (weightsA["TS02"] + weightsB["TS02"]) / 2
+    cnWts05 = (weightsA["TS05"] + weightsB["TS05"]) / 2
+    cnWts08 = (weightsA["TS08"] + weightsB["TS08"]) / 2
+    
+    Total_zts02 = 0
+    Total_zts05 = 0
+    Total_zts08 = 0
+    for z in all_neighbors(graph,cn):
+        parOrdernado = sorted([str(z),str(cn)])
+        row = database._pair[str(parOrdernado[0])+';'+str(parOrdernado[1])]
+        
+        if (len(row) > 0):
+            weightsZ = row[0]
+            Total_zts02 = Total_zts02 + weightsZ["TS02"]
+            Total_zts05 = Total_zts05 + weightsZ["TS05"]
+            Total_zts08 = Total_zts08 + weightsZ["TS08"]
+    
+    aats02 = 1 / (numpy.log10(Total_zts02)   + 0.00001)
+    aats05 = 1 / (numpy.log10(Total_zts05)   + 0.00001)
+    aats08 = 1 / (numpy.log10(Total_zts08)   + 0.00001)
+    
+    aaWts02 = aats02 * cnWts02
+    aaWts05 = aats05 * cnWts05
+    aaWts08 = aats08 * cnWts08
+    
+    return {'cnWts02': cnWts02, 'cnWts05': cnWts05, 'cnWts08': cnWts08, 'aaWts02': aaWts02, 'aaWts05':aaWts05, 'aaWts08':aaWts08 }
+    
+    
+    
 
 def generateWeights(graph, weightFile, param):
     pdb = Base(weightFile)
@@ -42,13 +99,110 @@ def generateWeights(graph, weightFile, param):
     pdb.commit()            
     return pdb
 
-def calculatingWeights(graph, nodesnotLinked, database):            
+def calculatingWeights(graph, nodesnotLinked, database, calculatingFile):
+    pdb = Base(calculatingFile)
+    pdb.create('node1', 'node2', 'cnWTS02','cnWTS05','cnWTS08', 'aaWTS02', 'aaWTS05', 'aaWTS08')
+    pdb.create_index('node1', 'node2')
+                
+    element = 0
+    qtyofNodesToProcess = len(nodesnotLinked)
     for pair in nodesnotLinked:
-        print database._pair[str(pair[0]) + ';' +str(pair[1])]
+        element = element+1
+        FormatingDataSets.printProgressofEvents(element, qtyofNodesToProcess, "Calculating features for nodes not liked: ")
+        neighbors_node1 = all_neighbors(graph, pair[0])
+        neighbors_node2 = all_neighbors(graph, pair[1])
+        len_neihbors_node1 = len(neighbors_node1)
+        len_neihbors_node2 = len(neighbors_node2)
+        CommonNeigbors = neighbors_node1.intersection(neighbors_node2)
+        CNWts02Feature = 0;
+        CNWts05Feature = 0;
+        CNWts08Feature = 0;
+        AAWts02Feature = 0;
+        AAWts05Feature = 0;
+        AAWts08Feature = 0;
+        for cn in CommonNeigbors:
+            item = get_partOfWeightCalculating(graph, database, pair, cn)
+            CNWts02Feature = CNWts02Feature + item['cnWts02'];
+            CNWts05Feature = CNWts05Feature + item['cnWts05'];
+            CNWts08Feature = CNWts08Feature + item['cnWts08'];
+            AAWts02Feature = AAWts02Feature + item['aaWts02'];
+            AAWts05Feature = AAWts05Feature + item['aaWts05'];
+            AAWts08Feature = AAWts08Feature + item['aaWts08'];
+        
+            
+        pdb.insert(str(pair[0]), str(pair[1]), CNWts02Feature, CNWts05Feature, CNWts08Feature, AAWts02Feature, AAWts05Feature, AAWts08Feature  )   
+    pdb.commit()
+    return pdb;
+
+def get_ordering(calcDb, topRank):
+    orderedResults = []
+    cnWTS02 = sorted( list({ 'node1': r['node1'], 'node2': r['node2'], 'value' : r['cnWTS02']} for r in calcDb)  , key=lambda value: value['value'], reverse=True)
+    cnWTS05 = sorted( list({ 'node1': r['node1'], 'node2': r['node2'], 'value' : r['cnWTS05']} for r in calcDb)  , key=lambda value: value['value'], reverse=True)
+    cnWTS08 = sorted( list({ 'node1': r['node1'], 'node2': r['node2'], 'value' : r['cnWTS08']} for r in calcDb)  , key=lambda value: value['value'], reverse=True)
+    aaWTS02 = sorted( list({ 'node1': r['node1'], 'node2': r['node2'], 'value' : r['aaWTS02']} for r in calcDb)  , key=lambda value: value['value'], reverse=True)
+    aaWTS05 = sorted( list({ 'node1': r['node1'], 'node2': r['node2'], 'value' : r['aaWTS05']} for r in calcDb)  , key=lambda value: value['value'], reverse=True)
+    aaWTS08 = sorted( list({ 'node1': r['node1'], 'node2': r['node2'], 'value' : r['aaWTS08']} for r in calcDb)  , key=lambda value: value['value'], reverse=True)
+    for item in range(topRank):
+        orderedResults.append({'cnWTS02' : cnWTS02[item], 'cnWTS05':cnWTS05[item], 'cnWTS08': cnWTS08[item], 'aaWTS02':aaWTS02[item], 'aaWTS05' : aaWTS05[item],'aaWTS08' : aaWTS08[item] })
+    return orderedResults
+
+
+def get_analyseNodesInFuture(ordering, TestGraph):
+    cnWTS02 = []
+    cnWTS05 = []
+    cnWTS08 = []
+    aaWTS02 = []
+    aaWTS05 = []
+    aaWTS08 = []
+        
+    for nodeToCheck in ordering:
+        if (TestGraph.has_edge(nodeToCheck['cnWTS02']['node1'],nodeToCheck['cnWTS02']['node2'])):
+            cnWTS02.append([  nodeToCheck['cnWTS02']['node1'],nodeToCheck['cnWTS02']['node2'], 1 ])
+        else:
+            cnWTS02.append([  nodeToCheck['cnWTS02']['node1'],nodeToCheck['cnWTS02']['node2'], 0 ])
+        
+        if (TestGraph.has_edge(nodeToCheck['cnWTS05']['node1'],nodeToCheck['cnWTS05']['node2'])):
+            cnWTS05.append([  nodeToCheck['cnWTS05']['node1'],nodeToCheck['cnWTS05']['node2'], 1 ])
+        else:
+            cnWTS05.append([  nodeToCheck['cnWTS05']['node1'],nodeToCheck['cnWTS05']['node2'], 0 ])
+        
+        if (TestGraph.has_edge(nodeToCheck['cnWTS08']['node1'],nodeToCheck['cnWTS08']['node2'])):
+            cnWTS08.append([  nodeToCheck['cnWTS08']['node1'],nodeToCheck['cnWTS08']['node2'], 1 ])
+        else:
+            cnWTS08.append([  nodeToCheck['cnWTS08']['node1'],nodeToCheck['cnWTS08']['node2'], 0 ])
+            
+        if (TestGraph.has_edge(nodeToCheck['aaWTS02']['node1'],nodeToCheck['aaWTS02']['node2'])):
+            aaWTS02.append([  nodeToCheck['aaWTS02']['node1'],nodeToCheck['aaWTS02']['node2'], 1 ])
+        else:
+            aaWTS02.append([  nodeToCheck['aaWTS02']['node1'],nodeToCheck['aaWTS02']['node2'], 0 ])
+        
+        if (TestGraph.has_edge(nodeToCheck['aaWTS05']['node1'],nodeToCheck['aaWTS05']['node2'])):
+            aaWTS05.append([  nodeToCheck['aaWTS05']['node1'],nodeToCheck['aaWTS05']['node2'], 1 ])
+        else:
+            aaWTS05.append([  nodeToCheck['aaWTS05']['node1'],nodeToCheck['aaWTS05']['node2'], 0 ])
+        
+        if (TestGraph.has_edge(nodeToCheck['aaWTS08']['node1'],nodeToCheck['aaWTS08']['node2'])):
+            aaWTS08.append([  nodeToCheck['aaWTS08']['node1'],nodeToCheck['aaWTS08']['node2'], 1 ])
+        else:
+            aaWTS08.append([  nodeToCheck['aaWTS08']['node1'],nodeToCheck['aaWTS08']['node2'], 0 ])
+    
+    
+    return {'cnWTS02' : len([result for n1,n2,result in cnWTS02 if result == 1]), 'cnWTS05' : len([result for n1,n2,result in cnWTS05 if result == 1]), 'cnWTS08' : len([result for n1,n2,result in cnWTS08 if result == 1]), 'aaWTS02' : len([result for n1,n2,result in aaWTS02 if result == 1]), 'aaWTS05' : len([result for n1,n2,result in aaWTS05 if result == 1]), 'aaWTS08' : len([result for n1,n2,result in aaWTS08 if result == 1]) }
+         
+
+
+def reading_Database(path):
+    adb = Base(path)
+    adb.open()
+    return adb
+
+
+
     
 
-
 def execution(configFile):
+   
+    
     #DEFINE THE FILE THAT WILL KEEP THE RESULT DATA
     resultFile = open(FormatingDataSets.get_abs_file_path(configFile + 'core03.txt'), 'w')
     
@@ -70,19 +224,22 @@ def execution(configFile):
     myparams.generating_Test_Graph()
     
     nodeSelection = NodeSelection(myparams.trainnigGraph, myparams.testGraph, util)
+    db = None
+    if not os.path.exists(FormatingDataSets.get_abs_file_path(util.trainnig_graph_file + '.base.pdl')):
+        db = generateWeights(myparams.trainnigGraph, FormatingDataSets.get_abs_file_path(util.trainnig_graph_file + '.base.pdl') , myparams)
+    else:
+        db = reading_Database(FormatingDataSets.get_abs_file_path(util.trainnig_graph_file + '.base.pdl'))
+    calcDb = None
+    if not os.path.exists(util.calculated_file + '.base.pdl'):
+        calcDb = calculatingWeights(myparams.trainnigGraph, nodeSelection.nodesNotLinked, db, util.calculated_file + '.base.pdl')
+    else:
+        calcDb = reading_Database(util.calculated_file + '.base.pdl')
+        
+    ordering = get_ordering(calcDb, len(nodeSelection.eNeW))
     
-    db = generateWeights(myparams.trainnigGraph, FormatingDataSets.get_abs_file_path(util.trainnig_graph_file + '.base.pdl') , myparams)
+    result = get_analyseNodesInFuture(ordering, myparams.testGraph)
     
-    calculatingWeights(myparams.trainnigGraph, nodeSelection.nodesNotLinked, db)
-    
-    
-    #calc = CalculatingTogetherOnlyNowell(myparams, nodeSelection.nodesNotLinked)
-    
-    #ordering = calc.ordering(len(nodeSelection.eNeW))
-    
-    #calc.AnalyseNodesInFuture(ordering, myparams.testGraph)
-    
-    #resultFile.write(repr(calc.get_TotalSucess()))
+    resultFile.write(repr(result))
     
     resultFile.write("\n")
 #        
@@ -102,26 +259,26 @@ def grqc():
     execution(configFile)
 
 def astroph():
-    configFile = 'data/configuration/arxiv/astroph/MetricaTemporal/config_NOWELLTS.txt'
+    configFile = 'data/configuration/arxiv/astroph/WeightedGraph/config_NOWELLTS.txt'
     execution(configFile)
 
 def condmat():
-    configFile = 'data/configuration/arxiv/condmat/MetricaTemporal/config_NOWELLTS.txt'
+    configFile = 'data/configuration/arxiv/condmat/WeightedGraph/config_NOWELLTS.txt'
     execution(configFile)
     
 def hepth():
-    configFile = 'data/configuration/arxiv/hepth/MetricaTemporal/config_NOWELLTS.txt'
+    configFile = 'data/configuration/arxiv/hepth/WeightedGraph/config_NOWELLTS.txt'
     execution(configFile)
 
 def hepph():
-    configFile = 'data/configuration/arxiv/hepph/MetricaTemporal/config_NOWELLTS.txt'
+    configFile = 'data/configuration/arxiv/hepph/WeightedGraph/config_NOWELLTS.txt'
     execution(configFile)
 
 if __name__ == '__main__':
     grqc()
-    #astroph()
-    #hepth()
-    #hepph()
-    #condmat()
+    astroph()
+    hepth()
+    hepph()
+    condmat()
     
     
