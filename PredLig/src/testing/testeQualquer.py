3288e9c2ca79fac736e81bad9f819c7321793747
Carlos Muniz <cptullio@gmail.com>
2016-07-01 23:22:29 -0300

    subindo conversao de arquivo para csv

--- PredLig/src/testing/testeQualquer.py
+++ PredLig/src/testing/testeQualquer.py
@@ -1,24 +1,71 @@
-'''
-Created on 21 de dez de 2015
+def reading_files(filename):
+    file = open(filename  , 'r')
+    data = eval(file.read())
+    return data
 
-@author: CarlosPM
-'''
-import networkx
-import matplotlib
+def converter_calculos():
+    file = '/results/grafos_nowell/grqc_1994_1999/CombinationLinear/ToAG/data.csv'
+    file2 = '/results/grafos_nowell/grqc_1994_1999/CombinationLinear/ToAG/data_convertido.csv'
+    
+    input = reading_files(file)
+    
+    output = open(file2, 'w')
+    
+    for item in input:
+        output.write(repr(item['node1']))
+        output.write(';')
+        output.write(repr(item['node2']))
+        output.write(';')
+        
+        output.write(repr(item['cn']))
+        output.write(';')
+        
+        output.write(repr(item['aas']))
+        output.write(';')
+        
+        output.write(repr(item['pa']))
+        output.write(';')
+        
+        output.write(repr(item['jc']))
+        output.write(';')
+        
+        output.write(repr(item['ts08']))
+        output.write(';')
+        
+        output.write(repr(item['ts05']))
+        output.write(';')
+        
+        output.write(repr(item['ts02']))
+        output.write('\n')
+        
+        
+    output.close()
+    
+
+
+def converter_analise():
+    file = '/results/grafos_nowell/grqc_1994_1999/CombinationLinear/ToAG/analysed.txt.allNodes.txt'
+    file2 = '/results/grafos_nowell/grqc_1994_1999/CombinationLinear/ToAG/analysed.txt.allNodes_convertido.csv'
+    
+    input = reading_files(file)
+    
+    output = open(file2, 'w')
+    
+    for item in input:
+        output.write(repr(item[0]))
+        output.write(';')
+        output.write(repr(item[1]))
+        output.write(';')
+        output.write(repr(item[2]))
+        output.write('\n')
+                
+        
+    output.close()
 
 
 if __name__ == '__main__':
-    result = []
-    
-    result.append({'n1':'no1','n2': 'no2', 'aas': 10, 'cn': 1,'jc': 2})
-    result.append({'n1':'no1','n2': 'no2', 'aas': 50, 'cn': 5,'jc': 2})
-    result.append({'n1':'no1','n2': 'no2', 'aas': 30, 'cn': 56,'jc': 2})
-    
-    s = sorted(result, key = lambda x: (x['cn']))
-    
-    print s[len(s)-1]
-    print result
-    
+    converter_calculos()
+    converter_analise()
     
     
     
